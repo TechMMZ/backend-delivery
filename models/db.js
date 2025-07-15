23e737a6afc83973284c306859cc29082f73a82e
Max Maza <maxmaza178@gmail.com>
2025-07-15 12:55:25 -0500

    Agregamos backend delivery bd

--- models/db.js
+++ models/db.js
@@ -5,22 +5,22 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
 
 //Manejar errores inesperados de la base de datos
 
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 pool.on('error', (err) => {
     console.error('Error inesperado en la conexión a la base de datos', err);
